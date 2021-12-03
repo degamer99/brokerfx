6cb3a095e2afb23d1c62d540a0559fbc703bb94e
degamer99 <75088933+degamer99@users.noreply.github.com>
2021-12-03 05:59:48 +0100

    Add files via upload

---
+++ script/scipts.js
@@ -0,0 +1,14 @@
+function userDataBase()
+{
+
+}
+function openSidebar()
+{
+    var sideBar = document.getElementById("sidebar");
+    sideBar.style.display = "block";
+}
+function closeSidebar()
+{
+    var sideBar = document.getElementById("sidebar");
+    sideBar.style.display = "none";
+}
